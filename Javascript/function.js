0f2befa8a6d6d12c1dad6385ced4d95b2cffc0a5
casadraboom <torrescasasjimenacasandra@gmail.com>
2024-02-15 11:55:08 -0600

    se agregaron varios tipos de imput

--- Javascript/function.js
+++ Javascript/function.js
@@ -1,4 +1,7 @@
 function changeColor(){
   document.getElementById("bomba").style.backgroundColor="green"; 
-
+  document.getElementById("bomba").textContent="hola";
 }
+
+function enviarMsg(){
+  alert("Hola amigo");
